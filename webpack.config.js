4b4a3bf7806624b7c6e0dbaa150ede8aaf1af8f0
jakweg <jakubek.weg@gmail.com>
2021-04-08 23:50:05 +0200

    Switched webpack to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
-  entry: {
-    main: "./src/main.ts",
-  },
-  output: {
-    path: path.resolve(__dirname, './docs'),
-    filename: "bundle.js"
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      { 
-        test: /\.tsx?$/,
-        loader: "ts-loader"
-      }
-    ]
-  }
-};
+    mode: 'production',
+    devtool: 'inline-source-map',
+    entry: {
+        main: './src/main.ts',
+    },
+    output: {
+        path: path.resolve(__dirname, './docs'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+            },
+        ],
+    },
+}
