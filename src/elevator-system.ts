c5db0512c153d99ed65261bde799d92eba756855
jakweg <jakubek.weg@gmail.com>
2021-04-08 23:40:25 +0200

    Reformatted code

--- src/elevator-system.ts
+++ src/elevator-system.ts
@@ -1,5 +1,5 @@
-import EventProducer from "./event-producer"
-import { generateUniqueId } from "./util"
+import EventProducer from './event-producer'
+import { generateUniqueId } from './util'
 
 export type EventType = 'elevator-added' | 'waiting-passenger-added' | 'passenger-taken' | 'passenger-dropped' | 'elevator-updated'
 
@@ -11,11 +11,11 @@ export interface ElevatorAndPassenger {
 export enum ElevatorDirection {
     Standing = 0,
     GoingUp = 1,
-    GoingDown = -1
+    GoingDown = -1,
 }
 
 export class Elevator {
-    public constructor(public readonly id: number,) { }
+    public constructor(public readonly id: number) {}
     public currentFloor: number = 0
     public direction: ElevatorDirection = ElevatorDirection.Standing
     public nextDirection: ElevatorDirection = ElevatorDirection.Standing
@@ -53,26 +53,23 @@ export default class ElevatorSystem extends EventProducer<EventType> {
         const passenger = <Passenger>{
             id: generateUniqueId(),
             direction: destinationFloor < initialFloor ? ElevatorDirection.GoingDown : ElevatorDirection.GoingUp,
-            name, initialFloor, destinationFloor
+            name,
+            initialFloor,
+            destinationFloor,
         }
         this.waitingPassengers.unshift(passenger)
         this.emit('waiting-passenger-added', passenger)
     }
 
     public commitNextStep() {
-
         const changedElevatorIds = new Set<number>()
-
-
 
         for (const elevator of this.elevators.values()) {
             if (elevator.direction !== ElevatorDirection.Standing) {
-
                 // move the elevator
                 changedElevatorIds.add(elevator.id)
 
                 elevator.currentFloor += elevator.direction as number
-
 
                 // drop passengers that wants to be on this floor
                 for (let i = elevator.passengers.length - 1; i >= 0; i--) {
@@ -84,12 +81,10 @@ export default class ElevatorSystem extends EventProducer<EventType> {
                 }
 
                 // if the elevator is at the limit floor this means it needs to change the direction
-                if (elevator.nextDirection !== ElevatorDirection.Standing
-                    && elevator.currentFloor === elevator.destinationLimit) {
+                if (elevator.nextDirection !== ElevatorDirection.Standing && elevator.currentFloor === elevator.destinationLimit) {
                     elevator.direction = elevator.nextDirection
                     elevator.nextDirection = ElevatorDirection.Standing
                     elevator.destinationLimit = Number.MAX_SAFE_INTEGER * elevator.direction
-
                 } else if (elevator.passengers.length === 0) {
                     if (elevator.nextDirection !== ElevatorDirection.Standing) {
                         // even thouh this elevator is empty it needs to go this way anyway
@@ -98,9 +93,11 @@ export default class ElevatorSystem extends EventProducer<EventType> {
                         let isSomeone = false
                         const tmp = elevator.currentFloor * elevator.direction
                         for (const passenger of this.waitingPassengers) {
-                            if (passenger.initialFloor * elevator.direction >= tmp
-                                && passenger.direction === elevator.direction
-                                && passenger.destinationFloor * elevator.direction <= elevator.destinationLimit * elevator.direction) {
+                            if (
+                                passenger.initialFloor * elevator.direction >= tmp &&
+                                passenger.direction === elevator.direction &&
+                                passenger.destinationFloor * elevator.direction <= elevator.destinationLimit * elevator.direction
+                            ) {
                                 isSomeone = true
                                 break
                             }
@@ -115,9 +112,11 @@ export default class ElevatorSystem extends EventProducer<EventType> {
                 // take passengers that want to go this way and are on this floor and they are within the limit
                 for (let i = this.waitingPassengers.length - 1; i >= 0; i--) {
                     const passenger = this.waitingPassengers[i]
-                    if (passenger.initialFloor === elevator.currentFloor
-                        && passenger.direction === elevator.direction
-                        && passenger.destinationFloor * elevator.direction < elevator.destinationLimit * elevator.direction) {
+                    if (
+                        passenger.initialFloor === elevator.currentFloor &&
+                        passenger.direction === elevator.direction &&
+                        passenger.destinationFloor * elevator.direction < elevator.destinationLimit * elevator.direction
+                    ) {
                         elevator.passengers.push(passenger)
                         this.waitingPassengers.splice(i, 1)
                         this.emit('passenger-taken', { passenger, elevator })
@@ -126,9 +125,6 @@ export default class ElevatorSystem extends EventProducer<EventType> {
             }
         }
 
-
-
-
         for (const elevator of this.elevators.values()) {
             if (elevator.direction == ElevatorDirection.Standing) {
                 // find a direction to go to
@@ -136,21 +132,25 @@ export default class ElevatorSystem extends EventProducer<EventType> {
                     // check if there is any elevator going to that persion, or will take it in the near future
                     let isAny = false
                     for (const otherElevator of this.elevators.values()) {
-
-                        if ((otherElevator.direction !== ElevatorDirection.Standing
-                            && passenger.initialFloor * otherElevator.direction > otherElevator.currentFloor * otherElevator.direction
-                            && passenger.destinationFloor * elevator.direction <= otherElevator.destinationLimit * otherElevator.direction
-                            && passenger.direction === otherElevator.direction)
-                            || (otherElevator.direction !== ElevatorDirection.Standing
-                                && otherElevator.nextDirection !== ElevatorDirection.Standing
-                                && passenger.initialFloor * otherElevator.direction <= otherElevator.destinationLimit * otherElevator.direction
-                                && passenger.direction === otherElevator.nextDirection)) {
-
+                        if (
+                            (otherElevator.direction !== ElevatorDirection.Standing &&
+                                passenger.initialFloor * otherElevator.direction > otherElevator.currentFloor * otherElevator.direction &&
+                                passenger.destinationFloor * elevator.direction <=
+                                    otherElevator.destinationLimit * otherElevator.direction &&
+                                passenger.direction === otherElevator.direction) ||
+                            (otherElevator.direction !== ElevatorDirection.Standing &&
+                                otherElevator.nextDirection !== ElevatorDirection.Standing &&
+                                passenger.initialFloor * otherElevator.direction <=
+                                    otherElevator.destinationLimit * otherElevator.direction &&
+                                passenger.direction === otherElevator.nextDirection)
+                        ) {
                             isAny = true
                             break
                         }
                     }
                     if (!isAny) {
+                        console.log('nope')
+
                         // if there are none, then make this elevator go for that person
                         elevator.direction = Math.sign(passenger.initialFloor - elevator.currentFloor)
                         elevator.nextDirection = Math.sign(passenger.destinationFloor - passenger.initialFloor)
@@ -160,9 +160,15 @@ export default class ElevatorSystem extends EventProducer<EventType> {
                         } else {
                             // it's good idea to find a person that is on lowest/heighest floor
                             let limit = passenger.initialFloor
-                            this.waitingPassengers.forEach((elevator.direction === ElevatorDirection.GoingUp) ?
-                                (p) => { if (p.initialFloor > limit) limit = p.initialFloor } :
-                                (p) => { if (p.initialFloor < limit) limit = p.initialFloor })
+                            this.waitingPassengers.forEach(
+                                elevator.direction === ElevatorDirection.GoingUp
+                                    ? (p) => {
+                                          if (p.initialFloor > limit) limit = p.initialFloor
+                                      }
+                                    : (p) => {
+                                          if (p.initialFloor < limit) limit = p.initialFloor
+                                      },
+                            )
 
                             elevator.destinationLimit = limit
                         }
--- src/main.ts
+++ src/main.ts
@@ -1,201 +1,178 @@
-import ElevatorSystem, {
-  Elevator,
-  Passenger,
-  ElevatorAndPassenger,
-  ElevatorDirection,
-} from "./elevator-system";
+import ElevatorSystem, { Elevator, Passenger, ElevatorAndPassenger, ElevatorDirection } from './elevator-system'
 
 // show main box when JS is enabled
-document.querySelector("main").style.display = null;
+document.querySelector('main').style.display = null
 
-const system = new ElevatorSystem();
+const system = new ElevatorSystem()
 // window.system = system
-const elevatorsListDiv = document.getElementById("elevators-list");
-const waitingPassengersListDiv = document.getElementById(
-  "waiting-passengers-list"
-);
+const elevatorsListDiv = document.getElementById('elevators-list')
+const waitingPassengersListDiv = document.getElementById('waiting-passengers-list')
 
 const formatDirection = (dir: ElevatorDirection): string => {
-  switch (dir) {
-    case ElevatorDirection.GoingUp:
-      return "up";
-    case ElevatorDirection.GoingDown:
-      return "down";
-    case ElevatorDirection.Standing:
-      return "none";
-  }
-};
+    switch (dir) {
+        case ElevatorDirection.GoingUp:
+            return 'up'
+        case ElevatorDirection.GoingDown:
+            return 'down'
+        case ElevatorDirection.Standing:
+            return 'none'
+    }
+}
 
 // DOM events:
-document.getElementById("add-elevator-btn").addEventListener("click", () => {
-  system.addNewElevator(+prompt("What floor is that elevator initially on?"));
-});
+document.getElementById('add-elevator-btn').addEventListener('click', () => {
+    system.addNewElevator(+prompt('What floor is that elevator initially on?'))
+})
 
-document.getElementById("add-passenger-btn").addEventListener("click", () => {
-  system.addNewPassenger({
-    name: prompt("Give a passenger name", "") || "",
-    initialFloor: +prompt("That floor is that passenger on?"),
-    destinationFloor: +prompt("That floor is that passenger going to go?"),
-  });
-});
+document.getElementById('add-passenger-btn').addEventListener('click', () => {
+    system.addNewPassenger({
+        name: prompt('Give a passenger name', '') || '',
+        initialFloor: +prompt('That floor is that passenger on?'),
+        destinationFloor: +prompt('That floor is that passenger going to go?'),
+    })
+})
 
-document
-  .getElementById("next-step-btn")
-  .addEventListener("click", () => system.commitNextStep());
+document.getElementById('next-step-btn').addEventListener('click', () => system.commitNextStep())
 
 // system events
-system.addEventListener("elevator-added", (elevator: Elevator) => {
-  const elevatorDiv = document.createElement("div");
-  elevatorDiv.id = `elevator-id-${elevator.id}`;
-  elevatorDiv.classList.add("elevator");
+system.addEventListener('elevator-added', (elevator: Elevator) => {
+    const elevatorDiv = document.createElement('div')
+    elevatorDiv.id = `elevator-id-${elevator.id}`
+    elevatorDiv.classList.add('elevator')
 
-  //   {
-  //     const el = document.createElement("div");
-  //     el.classList.add("elevator-id-value");
-  //     el.innerText = `${elevator.id}`;
-  //     elevatorDiv.appendChild(el);
-  //   }
-  //   {
-  //     const el = document.createElement("div");
-  //     el.classList.add("elevator-id-title");
-  //     el.innerText = `Elevator ID`;
-  //     elevatorDiv.appendChild(el);
-  //   }
+    //   {
+    //     const el = document.createElement("div");
+    //     el.classList.add("elevator-id-value");
+    //     el.innerText = `${elevator.id}`;
+    //     elevatorDiv.appendChild(el);
+    //   }
+    //   {
+    //     const el = document.createElement("div");
+    //     el.classList.add("elevator-id-title");
+    //     el.innerText = `Elevator ID`;
+    //     elevatorDiv.appendChild(el);
+    //   }
 
-  {
-    const el = document.createElement("div");
-    el.classList.add("current-floor-value");
-    el.innerText = `${elevator.currentFloor}`;
-    elevatorDiv.appendChild(el);
-  }
-  {
-    const el = document.createElement("div");
-    el.innerText = `Current floor`;
-    elevatorDiv.appendChild(el);
-  }
+    {
+        const el = document.createElement('div')
+        el.classList.add('current-floor-value')
+        el.innerText = `${elevator.currentFloor}`
+        elevatorDiv.appendChild(el)
+    }
+    {
+        const el = document.createElement('div')
+        el.innerText = `Current floor`
+        elevatorDiv.appendChild(el)
+    }
 
-  {
-    const el = document.createElement("div");
-    el.classList.add("direction-value");
-    el.innerText = `${formatDirection(elevator.direction)}`;
-    elevatorDiv.appendChild(el);
-  }
-  {
-    const el = document.createElement("div");
-    el.innerText = `Direction`;
-    elevatorDiv.appendChild(el);
-  }
+    {
+        const el = document.createElement('div')
+        el.classList.add('direction-value')
+        el.innerText = `${formatDirection(elevator.direction)}`
+        elevatorDiv.appendChild(el)
+    }
+    {
+        const el = document.createElement('div')
+        el.innerText = `Direction`
+        elevatorDiv.appendChild(el)
+    }
 
-  // {
-  //     const el = document.createElement('div')
-  //     el.classList.add('limit-floor-value')
-  //     el.innerText = `${elevator.destinationLimit}`
-  //     elevatorDiv.appendChild(el)
-  // }
-  // {
-  //     const el = document.createElement('div')
-  //     el.innerText = `Floor limit`
-  //     elevatorDiv.appendChild(el)
-  // }
+    // {
+    //     const el = document.createElement('div')
+    //     el.classList.add('limit-floor-value')
+    //     el.innerText = `${elevator.destinationLimit}`
+    //     elevatorDiv.appendChild(el)
+    // }
+    // {
+    //     const el = document.createElement('div')
+    //     el.innerText = `Floor limit`
+    //     elevatorDiv.appendChild(el)
+    // }
 
-  // {
-  //     const el = document.createElement('div')
-  //     el.classList.add('next-direction-value')
-  //     el.innerText = `${formatDirection(elevator.nextDirection)}`
-  //     elevatorDiv.appendChild(el)
-  // }
-  // {
-  //     const el = document.createElement('div')
-  //     el.innerText = `Next direction`
-  //     elevatorDiv.appendChild(el)
-  // }
+    // {
+    //     const el = document.createElement('div')
+    //     el.classList.add('next-direction-value')
+    //     el.innerText = `${formatDirection(elevator.nextDirection)}`
+    //     elevatorDiv.appendChild(el)
+    // }
+    // {
+    //     const el = document.createElement('div')
+    //     el.innerText = `Next direction`
+    //     elevatorDiv.appendChild(el)
+    // }
 
-  {
-    const el = document.createElement("div");
-    el.classList.add("passengers-inside-list");
-    elevatorDiv.appendChild(el);
-  }
+    {
+        const el = document.createElement('div')
+        el.classList.add('passengers-inside-list')
+        elevatorDiv.appendChild(el)
+    }
 
-  elevatorsListDiv.appendChild(elevatorDiv);
-});
+    elevatorsListDiv.appendChild(elevatorDiv)
+})
 
-system.addEventListener("elevator-updated", (elevator: Elevator) => {
-  const elevatorDiv = document.getElementById(`elevator-id-${elevator.id}`);
-  (elevatorDiv.querySelector(
-    ".current-floor-value"
-  ) as HTMLDivElement).innerText = `${elevator.currentFloor}`;
-  (elevatorDiv.querySelector(
-    ".direction-value"
-  ) as HTMLDivElement).innerText = `${formatDirection(elevator.direction)}`;
-  // (elevatorDiv.querySelector('.limit-floor-value') as HTMLDivElement).innerText = `${elevator.destinationLimit}`;
-  // (elevatorDiv.querySelector('.next-direction-value') as HTMLDivElement).innerText = `${formatDirection(elevator.nextDirection)}`;
-});
+system.addEventListener('elevator-updated', (elevator: Elevator) => {
+    const elevatorDiv = document.getElementById(`elevator-id-${elevator.id}`)
+    ;(elevatorDiv.querySelector('.current-floor-value') as HTMLDivElement).innerText = `${elevator.currentFloor}`
+    ;(elevatorDiv.querySelector('.direction-value') as HTMLDivElement).innerText = `${formatDirection(elevator.direction)}`
+    // (elevatorDiv.querySelector('.limit-floor-value') as HTMLDivElement).innerText = `${elevator.destinationLimit}`;
+    // (elevatorDiv.querySelector('.next-direction-value') as HTMLDivElement).innerText = `${formatDirection(elevator.nextDirection)}`;
+})
 
-system.addEventListener("waiting-passenger-added", (passenger: Passenger) => {
-  const elevatorDiv = document.createElement("div");
-  elevatorDiv.id = `waiting-passenger-id-${passenger.id}`;
-  elevatorDiv.classList.add("waiting-passenger");
+system.addEventListener('waiting-passenger-added', (passenger: Passenger) => {
+    const elevatorDiv = document.createElement('div')
+    elevatorDiv.id = `waiting-passenger-id-${passenger.id}`
+    elevatorDiv.classList.add('waiting-passenger')
 
-  {
-    const el = document.createElement("div");
-    el.innerText = `${passenger.name}`;
-    elevatorDiv.appendChild(el);
-  }
-  {
-    const el = document.createElement("div");
-    el.innerText = `Name`;
-    elevatorDiv.appendChild(el);
-  }
+    {
+        const el = document.createElement('div')
+        el.innerText = `${passenger.name}`
+        elevatorDiv.appendChild(el)
+    }
+    {
+        const el = document.createElement('div')
+        el.innerText = `Name`
+        elevatorDiv.appendChild(el)
+    }
 
-  {
-    const el = document.createElement("div");
-    el.innerText = `${passenger.initialFloor}`;
-    elevatorDiv.appendChild(el);
-  }
-  {
-    const el = document.createElement("div");
-    el.innerText = `Waiting at`;
-    elevatorDiv.appendChild(el);
-  }
+    {
+        const el = document.createElement('div')
+        el.innerText = `${passenger.initialFloor}`
+        elevatorDiv.appendChild(el)
+    }
+    {
+        const el = document.createElement('div')
+        el.innerText = `Waiting at`
+        elevatorDiv.appendChild(el)
+    }
 
-  {
-    const el = document.createElement("div");
-    el.innerText = `${passenger.destinationFloor}`;
-    elevatorDiv.appendChild(el);
-  }
-  {
-    const el = document.createElement("div");
-    el.innerText = `Destination floor`;
-    elevatorDiv.appendChild(el);
-  }
+    {
+        const el = document.createElement('div')
+        el.innerText = `${passenger.destinationFloor}`
+        elevatorDiv.appendChild(el)
+    }
+    {
+        const el = document.createElement('div')
+        el.innerText = `Destination floor`
+        elevatorDiv.appendChild(el)
+    }
 
-  waitingPassengersListDiv.appendChild(elevatorDiv);
-});
+    waitingPassengersListDiv.appendChild(elevatorDiv)
+})
 
-system.addEventListener(
-  "passenger-taken",
-  ({ passenger, elevator }: ElevatorAndPassenger) => {
-    document.getElementById(`waiting-passenger-id-${passenger.id}`).remove();
-    const listOfInsiders = document.querySelector(
-      `#elevator-id-${elevator.id} .passengers-inside-list`
-    );
+system.addEventListener('passenger-taken', ({ passenger, elevator }: ElevatorAndPassenger) => {
+    document.getElementById(`waiting-passenger-id-${passenger.id}`).remove()
+    const listOfInsiders = document.querySelector(`#elevator-id-${elevator.id} .passengers-inside-list`)
 
-    const el = document.createElement("div");
-    el.id = `passenger_${passenger.id}`;
-    el.classList.add("passenger-inside");
-    el.innerText = `${passenger.name}`;
-    listOfInsiders.appendChild(el);
-  }
-);
+    const el = document.createElement('div')
+    el.id = `passenger_${passenger.id}`
+    el.classList.add('passenger-inside')
+    el.innerText = `${passenger.name}`
+    listOfInsiders.appendChild(el)
+})
 
-system.addEventListener(
-  "passenger-dropped",
-  ({ passenger, elevator }: ElevatorAndPassenger) => {
-    document
-      .querySelector(`#elevator-id-${elevator.id} #passenger_${passenger.id}`)
-      .remove();
-  }
-);
+system.addEventListener('passenger-dropped', ({ passenger, elevator }: ElevatorAndPassenger) => {
+    document.querySelector(`#elevator-id-${elevator.id} #passenger_${passenger.id}`).remove()
+})
 
 // system.addNewElevator();
 // system.addNewElevator();
